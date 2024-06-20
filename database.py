9f25de38945f87162822dbf8901fff0205c99bb9
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-20 17:51:46 -0600

    added estatus configs and fix typo on execute

--- database.py
+++ database.py
@@ -1,24 +1,37 @@
-from sqlalchemy import Column, Integer, String, Enum, Select, Insert, Update, Delete, CursorResult, Table, MetaData
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Enum,
+    Select,
+    Insert,
+    Update,
+    Delete,
+    CursorResult,
+    Table,
+    MetaData,
+)
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Any
 import enum
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-    class_=AsyncSession
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
 )
 
 Base = declarative_base()
 
 metadata = MetaData()
+
+
 # Tablas
 class AfinidadMagicaEnum(enum.Enum):
     Oscuridad = "Oscuridad"
@@ -28,15 +41,23 @@ class AfinidadMagicaEnum(enum.Enum):
     Viento = "Viento"
     Tierra = "Tierra"
 
+
+class EstatusEnum(enum.Enum):
+    procesando = "Procesando"
+    aprobada = "Aprobada"
+    denegada = "Denegada"
+
+
 solicitudes = Table(
     "solicitudes",
-    metadata,   
+    metadata,
     Column("nombre", String(20), nullable=False),
     Column("apellido", String(20), nullable=False),
     Column("identificacion", String(10), unique=True, nullable=False),
     Column("edad", Integer, nullable=False),
     Column("afinidad_magica", Enum(AfinidadMagicaEnum), nullable=False),
-    schema="ia"
+    Column("estatus", Enum(EstatusEnum), nullable=False),
+    schema="ia",
 )
 
 magos = Table(
@@ -48,16 +69,17 @@ magos = Table(
     Column("edad", Integer, nullable=False),
     Column("afinidad_magica", Enum(AfinidadMagicaEnum), nullable=False),
     Column("treboles_grimorio", Integer, nullable=False),
-    schema="ia"
+    schema="ia",
 )
+
 
 # Abstracciones para ejecturar queries
 async def fetch_all(select_query: Select | Insert | Update) -> dict[str, Any] | None:
     async with engine.begin() as conn:
-        cursor: CursorResult = await conn.exucute(select_query)
+        cursor: CursorResult = await conn.execute(select_query)
         return [result._asdict() for result in cursor.all()]
 
 
-async def execute(select_query: Delete | Insert | Update) -> None:
+async def execute(query: Delete | Insert | Update) -> None:
     async with engine.begin() as conn:
-        await conn.exucute(select_query)
+        await conn.execute(query)
