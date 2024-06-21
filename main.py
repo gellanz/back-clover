cde280792dad5e49c266da45a7ceb296de200299
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-20 22:50:48 -0600

    remove double import

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ import uvicorn
 # To run the application, use the command:
 # uvicorn your_script_name:app --reload
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
