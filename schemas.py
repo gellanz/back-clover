89a986fd36b021f26876c5d6e2b3947c24ad3160
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-20 18:56:50 -0600

    Mensajes de transacciones finalizadas

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from models import Solicitud
+
+
+class NuevaSolicitud(BaseModel):
+    status_code: int = 200
+    message: str
+    data: Solicitud
+
+
+class TransaccionCompletada(BaseModel):
+    status_code: int = 200
+    message: str
+    id: str
