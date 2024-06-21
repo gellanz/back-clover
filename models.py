7bf82e52ee83fad97cfbb135b2e5b5a3999d53cb
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-20 18:56:30 -0600

    modificacion de los magos que obtuvieron grimorio

--- models.py
+++ models.py
@@ -9,5 +9,6 @@ class Solicitud(BaseModel):
     edad: Annotated[int, Field(strict=True, gt=15, lt=99, description="Edad del mago aspirante, generalmente hacen la solicitud a los 15")]
     afinidad_magica: Literal['Oscuridad', 'Luz', 'Fuego', 'Agua', 'Viento', 'Tierra'] = Field(..., description="Afinidad Mágica")
 
-class Mago(Solicitud):
+class Mago(BaseModel):
+    identificacion: Annotated[str, StringConstraints(pattern='^[a-zA-Z0-9]+$', max_length=10)] = Field(..., description="Identificación: números y letras, máximo 10 caracteres.")
     treboles_grimorio: Annotated[int, Field(strict=True, gt=0, lt=6)]
