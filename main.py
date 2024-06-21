121669a1c2b8a6c8fd318224ff956f105312afb7
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-21 10:33:36 -0600

    0.0.0.0

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 # To run the application, use the command:
 # uvicorn your_script_name:app --reload
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
