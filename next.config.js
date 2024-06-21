b491d1229e1220ecc6ec734497c6a941fcec22ae
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-21 10:00:04 -0600

    modified paths

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ const nextConfig = {
           source: "/:path*",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/:path*"
-              : "/api/",
+              ? "http://0.0.0.0:8000/:path*"
+              : "/",
         },
         {
           source: "/docs",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/docs"
+              ? "http://0.0.0.0:8000/docs"
               : "/docs",
         },
         {
           source: "/openapi.json",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/openapi.json"
+              ? "http://0.0.0.0:8000/openapi.json"
               : "/openapi.json",
         },
       ];
