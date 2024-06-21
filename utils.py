d1a26ab042536bbc3ab2c75817e4f09fd7ec70c2
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2024-06-20 18:57:12 -0600

    funciones para generar grimorio y validaciones

---
+++ utils.py
@@ -0,0 +1,26 @@
+from database import solicitudes, magos, fetch_all, execute
+from sqlalchemy import select, insert
+import random
+
+
+def generar_grimorio():
+    numbers = [1, 2, 3, 4, 5]
+    # probabilidades para cada numero de treboles. Solo uno de cada 100 sacarian un grimorio de 5 hojas
+    weights = [0.15, 0.4, 0.3, 0.14, 0.01]
+    return random.choices(numbers, weights)[0]
+
+
+async def insertar_nuevo_mago(id):
+    insert_new_mage_query = insert(magos).values(
+        identicacion=id, treboles_grimorio=generar_grimorio()
+    )
+    await execute(insert_new_mage_query)
+
+
+async def validar_existencia_solicitud(id: str):
+    query = (
+        select("*").select_from(solicitudes).where(solicitudes.c.identificacion == id)
+    )
+    if fetch_all(query):
+        return True
+    return False
